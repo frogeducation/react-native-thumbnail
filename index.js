a260b6718f9f4efa805a4888d3ae52562b6a5265
David Godfrey <david.godfrey@frogeducation.com>
2020-02-17 17:40:55 +0000

    fix: add delay to avoid black thumbnails
    thumbnails were generated and ready for consumption in native, but were
    failing to load in JS. Adding a short delay fixes the issue.

--- index.js
+++ index.js
@@ -3,4 +3,17 @@ import { NativeModules } from 'react-native';
 
 const { RNThumbnail } = NativeModules;
 
+(() => {
+    const oldMethod = RNThumbnail.get;
+    RNThumbnail.get = media => oldMethod(media)
+        .then(result =>
+            new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(result);
+                }, 100)
+            })
+        )
+})();
+
 export default RNThumbnail;
+
